1c29f99997e95628c488cac422fb26047ad85829
iainmwallace <iain.m.wallace@gmail.com>
2017-10-29 23:06:00 -0400

    removed foldername option, didn't work as expected

--- R/data2gcs.R
+++ R/data2gcs.R
@@ -1,4 +1,4 @@
-data2gcs <- function(my_data,my_file_name,my_bucket,my_foldername,my_metadata, chunk_size=100000) {
+data2gcs <- function(my_data,my_file_name,my_bucket,my_metadata, chunk_size=100000) {
   #' @title Store data as collection of zipped json files on cloud storage
   #' @description  Split a dataset into many small json files and upload to a
   #'     bucket. All files generated in as part of the function call will
@@ -10,7 +10,6 @@ data2gcs <- function(my_data,my_file_name,my_bucket,my_foldername,my_metadata, c
   #' @import readr
   #' @param my_data Dataset to upload to gcs. It must pass a validation test
   #' @param my_file_name Main name for each json file
-  #' @param my_foldername Folder to use in the bucket
   #' @param my_metadata List of metadata to be associated with the uploaded files
   #' @param bucket Bucket to store the files in
   #' @param chunk_size Number of rows per json file, ideally <5mb per file
@@ -36,7 +35,7 @@ data2gcs <- function(my_data,my_file_name,my_bucket,my_foldername,my_metadata, c
   file_pattern <- paste0(my_file_name, "_", UUIDgenerate(), "_")
 
   cat("Preparing temporary json files for uploading\n")
-  pb <- txtProgressBar(min = 1,
+  pb <- txtProgressBar(min = 0,
                        max = length(chunks),
                        style = 3)
 
@@ -64,8 +63,7 @@ data2gcs <- function(my_data,my_file_name,my_bucket,my_foldername,my_metadata, c
     upload_details[[i]] <-
       gcs_upload(my_filename,
                  bucket = my_bucket,
-                 object_metadata = meta,
-                 name=paste0(my_foldername,"/",files[i]))
+                 object_metadata = meta)
     setTxtProgressBar(pb, i)
   }
 
